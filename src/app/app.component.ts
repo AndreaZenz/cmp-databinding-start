51cd82359d542f9b0cee52c73e38dd70e356892a
AndreaValeri <76973159+AndreaZenz@users.noreply.github.com>
2021-10-28 12:27:29 +0200

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,27 @@
+import { Component, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+  encapsulation: ViewEncapsulation.Emulated // None
+})
+export class AppComponent {
+  serverElements = [{ type: 'server', name: 'Testserver', content: 'Just a text' }];
+
+  onServerAdded(serverData:{serverName: string, serverContent:string}) {
+    this.serverElements.push({
+      type: 'server',
+      name: serverData.serverName,
+      content: serverData.serverContent
+    });
+  }
+
+  onBlueprintAdded(blueprintData: {serverName:string, serverContent:string}) {
+    this.serverElements.push({
+      type: 'blueprint',
+      name: blueprintData.serverName,
+      content: blueprintData.serverContent
+    });
+  }
+}
